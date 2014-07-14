e10bbe47fb603690cf08017787f9f85ab881249a
Florian Cechi <florian.cechi@gmail.com>
2014-07-14 16:14:32 +0300

    user/email/lowercase

--- app/models/user.js
+++ app/models/user.js
@@ -14,7 +14,7 @@ var mongoose = require('mongoose')
 
 var UserSchema = new Schema({
     name: String,
-    email: String,
+    email: {type: "String", lowercase: true},
     username: String,
     provider: String,
     hashed_password: String,
